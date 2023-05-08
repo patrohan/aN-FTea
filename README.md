8144248f473f4221099b235e606651c49527328d
Rohan Patwardhan <rohanpat30@gmail.com>
2023-05-07 20:35:51 -0700

    Add files via upload
    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# aN-FTea
-NFT marketplace to buy and sell NFT's
+Project Title - aN-FTea
+
+Team Members - 
+Krishna Bhatia   (886198043) krishna17897@csu.fullerton.edu
+Rohan Patwardhan (885944405) rohanpat30@csu.fullerton.edu
+
+How to run the project - 
+1. Download and Unzip the rar file
+2. Open folder in VS Code or go to the location via terminal
+3. run 'npm install'
+4. Configure your hardhat.config.js with api key and private key of your testnet.(Prefered network - Sepolia)
+4. run 'npm start'
+
+Improvements - 
+1. UI improvements
+2. Rather than storing data on a traditional database, we used IPFS protocols to store data on a P2P network. Used Piniata for IPFS storage.
+3. Deploying smartcontracts on Truffle/Ganache and Hardhat.
+4. Minting digital arta as Non Fungible Tokens.
+5. Buying/Selling/Listing all the NFTs on the marketplace.
+6. Used Goerli and Sepolia testnet.
+
---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
+      //accounts: [process.env.privateKey]
+    },
+    matic: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
+      //accounts: [process.env.privateKey]
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/nXAXxcJUtCnvC1HsOZJFT_ixmej6ayly",
+      accounts: [ "dc58d82dd132ac8dfefea3eb034089e85cf1d4e114d45887ecf0d8f77e132a5e" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
